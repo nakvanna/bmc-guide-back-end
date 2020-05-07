874e462b62e742bce4dbb6673d7dec17ce5fadfe
tunchamroeun <chamroeuntun25@outlook.com>
2020-05-07 22:29:56 +0700

    update passport

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        
+
     }
 
     /**
@@ -35,7 +35,6 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        
         $input = $request->all();
         $request->validate([
             'category' => 'required'
